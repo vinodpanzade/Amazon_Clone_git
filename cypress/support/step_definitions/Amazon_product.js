55b5b6104a663fb3c4fc221863f2d5799055e75e
vinodpanzade <vinodpanzade64@gmail.com>
2026-02-28 20:36:37 +0530

    changes are done

--- cypress/support/step_definitions/Amazon_product.js
+++ cypress/support/step_definitions/Amazon_product.js
@@ -1,16 +1,16 @@
 import { Given, Then } from "@badeball/cypress-cucumber-preprocessor";
-import Amazon_product from "../../Pages/Amazon_product"
+import Amazon_product from "../../Pages/Amazon_product";
 
 const amazon_prod = new Amazon_product();
 
-Given ("I open the homepage",()=>{
-    amazon_prod.visit();
-})
+Given("I open the homepage", () => {
+  amazon_prod.visit();
+});
 
-Then("verify the product",()=>{
-     amazon_prod.product_search();
-})
+Then("verify the product", () => {
+  amazon_prod.product_search();
+});
 
-Then("Verify other product list",()=>{
-    amazon_prod.product_list()
-})
+Then("Verify other product list", () => {
+  amazon_prod.product_list();
+});
